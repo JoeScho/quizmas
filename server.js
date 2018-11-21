45a8c979545dad4dc3a0d4be73ab1ad046e85ecb
João Guimarães <joao.guimaraes@yld.io>
2018-11-21 01:42:30 +0000

    feat(questions): dynamic questions feat(leaderboard): tweaked the styles

--- server.js
+++ server.js
@@ -10,29 +10,31 @@ const { Users } = require('./utils/users');
 
 const publicPath = path.join(__dirname, './public');
 const port = process.env.PORT || 3000;
-var app = express();
-var server = http.createServer(app);
-var io = socketIO(server);
-var users = new Users();
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+const users = new Users();
 const QUIZ = 'DAQZ';
 
 app.use(express.static(publicPath));
 
-
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  const startTimer = answer => {
-    setTimeout(() => {
-      io.to(QUIZ).emit('timeup', { answer });
+  let timeout;
+
+  const startTimer = (answer, list, questionId) => {
+    timeout = setTimeout(() => {
+      io.to(QUIZ).emit('timesup', { answer, list, questionId });
     }, 5000)
   };
 
   socket.on('answer', (params, callback) => {
     console.log(`got answer ${params.answer} for ${JSON.stringify(users.getUser(socket.id))}`);
-    const { correct } = questions[0];
+    const { correct } = questions[params.questionId];
     if(params.answer === correct) {
       users.addPoint(socket.id);
+      console.log(`correct answer for ${JSON.stringify(users.getUser(socket.id))}`);
     }
 
     io.to(QUIZ).emit('updateUserList', users.getUserList(QUIZ));
@@ -56,7 +58,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    var user = users.getUser(socket.id);
+    const user = users.getUser(socket.id);
 
     if (user && isRealString(message.text)) {
       io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
@@ -66,14 +68,16 @@ io.on('connection', (socket) => {
 
   // admin started game
   socket.on('startGame', (message, callback) => {
-    const { correct, ...q1 } = questions[0];
-    io.to("DAQZ").emit('gameStarted', q1);
-    startTimer(correct);
+    const questionId = Math.floor(Math.random() * Math.floor(questions.length));
+    const { correct, ...list } = questions[questionId];
+    io.to("DAQZ").emit('gameStarted', list);
+    clearTimeout(timeout);
+    startTimer(correct, list, questionId);
     callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    var user = users.getUser(socket.id);
+    const user = users.getUser(socket.id);
 
     if (user) {
       io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
@@ -81,7 +85,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    var user = users.removeUser(socket.id);
+    const user = users.removeUser(socket.id);
 
     if (user) {
       io.to(user.room).emit('updateUserList', users.getUserList(user.room));
